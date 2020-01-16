b7ca1ec0b2e27675a2080b238d24087cd23d65d5
epwalsh <epwalsh10@gmail.com>
2020-01-16 11:34:14 -0800

    use lowercase name

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -13,7 +13,7 @@ use crate::error::Error;
 /// use celery::{task, Error};
 ///
 /// #[task(name = "add")]
-/// fn Add(x: i32, y: i32) -> i32 {
+/// fn add(x: i32, y: i32) -> i32 {
 ///     x + y
 /// }
 /// ```
