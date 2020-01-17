f1706bf635221532369e518797fcd86655748c34
epwalsh <epwalsh10@gmail.com>
2020-01-17 09:47:15 -0800

    update docs

--- src/task/mod.rs
+++ src/task/mod.rs
@@ -48,17 +48,61 @@ use crate::error::Error;
 /// }
 /// ```
 ///
+/// Within the [Celery protocol](https://docs.celeryproject.org/en/latest/internals/protocol.html#version-2)
+/// the task parameters can be treated as either `args` (positional) or `kwargs` (key-word based).
+/// So, for example, from Python the `add` task could be called like
+///
+/// ```python
+/// celery_app.send_task("add", args=[1, 2])
+/// ```
+///
+/// or
+///
+/// ```python
+/// celery_app.send_task("add", kwargs={"x": 1, "y": 2})
+/// ```
+///
+/// # Making task parameters optional
+///
+/// You can provide default values for task parameters through the
+/// [deserialization mechanism](https://serde.rs/attr-default.html). Currently this means
+/// you'll have to implement the task manually as opposed to using the `#[task]` macro.
+///
+/// So if we wanted to make the `y` parameter in the `add` task optional with a default
+/// value of 0, we could add the `#[serde(default)]` macro to the `y` field:
+///
+/// ```rust
+/// # use async_trait::async_trait;
+/// # use serde::{Serialize, Deserialize};
+/// # use celery::{Task, Error};
+/// #[allow(non_camel_case_types)]
+/// #[derive(Serialize, Deserialize)]
+/// struct add {
+///     x: i32,
+///     #[serde(default)]
+///     y: i32,
+/// }
+/// # #[async_trait]
+/// # impl Task for add {
+/// #     const NAME: &'static str = "add";
+/// #     const ARGS: &'static [&'static str] = &["x", "y"];
+/// #     type Returns = i32;
+/// #     async fn run(&mut self) -> Result<Self::Returns, Error> {
+/// #         Ok(self.x + self.y)
+/// #     }
+/// # }
+/// ```
+///
 /// # Error handling
 ///
-/// As shown above, a task tries wrapping the return value in `Result<Self::Returns, Error>`
-/// when it is executed. Therefore the recommended way to propogate errors is to use the `?`
-/// operator within the task body.
+/// As demonstrated above, the `#[task]` macro tries wrapping the return value in `Result<Self::Returns, Error>`
+/// when it is ran. Therefore the recommended way to propogate errors when defining a task with
+/// `#[task]` is to use `.context("...")?` on `Result` types within the task body.
 ///
 /// For example:
 ///
 /// ```rust
-/// # use celery::task;
-/// use failure::ResultExt;
+/// use celery::{task, ResultExt};
 ///
 /// #[task(name = "add")]
 /// fn read_some_file() -> String {
@@ -68,23 +112,9 @@ use crate::error::Error;
 /// }
 /// ```
 ///
-/// # Error handling
-///
-/// As shown above, a task tries wrapping the return value in `Result<Self::Returns, Error>`
-/// when it is executed, so it is actually valid to return a `Result` directly
-/// and also to use the `?` operator within the task body.
-///
-/// For example:
-///
-/// ```rust
-/// # use celery::task;
-/// use failure::ResultExt;
-///
-/// #[task(name = "add")]
-/// fn read_some_file() -> String {
-///     tokio::fs::read_to_string("some_file").await.context("File does not exist")?
-/// }
-/// ```
+/// The `.context` method on a `Result` comes from the [`ResultExt`](trait.ResultExt.html) trait.
+/// This is used to provide additional human-readable context to the error and also
+/// to convert it into the expected [`Error`](struct.Error.html) type.
 #[async_trait]
 pub trait Task: Send + Sync + Serialize + for<'de> Deserialize<'de> {
     /// The name of the task. When a task is registered it will be registered with this name.
