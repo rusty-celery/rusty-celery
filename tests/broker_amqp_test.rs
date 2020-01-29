0999e4d5732e338d50cb3bb1c6aebf0096b25725
epwalsh <epwalsh10@gmail.com>
2020-01-28 17:10:51 -0800

    add integration test

--- tests/broker_amqp_test.rs
+++ tests/broker_amqp_test.rs
@@ -1,4 +1,19 @@
-//! Integration tests for AMQP broker.
+#![allow(non_upper_case_globals)]
 
-#[test]
-fn foo() {}
+use celery::{celery_app, task, AMQPBroker};
+
+#[task]
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+celery_app!(
+    my_app,
+    AMQPBroker { std::env::var("AMQP_ADDR").unwrap() },
+    tasks = [add],
+);
+
+#[tokio::test]
+async fn test_send_task() {
+    assert!(my_app.send_task(add(1, 2)).await.is_ok());
+}
